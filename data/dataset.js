3f067cbdb871687d2b3653e069aff45f788da849
kcamelo <153109575+kcamelo@users.noreply.github.com>
2024-01-16 22:36:23 -0300

    Updates

---
+++ data/dataset.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    "id": "ada-lovelace",
+    "name": "Ada Lovelace",
+    "shortDescription": "Pionera en la programación, primera programadora de la historia.",
+    "description": "Una visionaria matemática británica del siglo XIX, es aclamada como la primera programadora de la historia. Su colaboración con Charles Babbage en la creación de la Máquina Analítica la llevó a desarrollar algoritmos, incluyendo el famoso \"algoritmo de Bernoulli\", destinados a ser procesados por una máquina. Su visión pionera la convirtió en una figura fundamental en la informática y la inteligencia artificial. Lovelace demostró que las computadoras podían ir más allá de simples cálculos matemáticos, anticipando un mundo donde la programación sería esencial en la resolución de problemas complejos, dejando una huella imborrable en la ciencia y la tecnología.",
+    "imageUrl": "https://laboratoria-dataverse-talks.netlify.app/public/ada-lovelace.jpg",
+    "facts": {
+      "yearOfBirth": "1815",
+      "yearOfDeath": "1852",
+      "birthPlace": "Londres, Reino Unido",
+      "mainField": "Ciencia de la Computación"
+    },
+    "extraInfo": {
+      "imageSource": "https://www.bing.com/images/create/genera-una-imagen-de-ada-lovelace2c-la-primera-prog/650a1eff8ff04b88833d5cdef0309441"
+    }
+  },
+
+];
