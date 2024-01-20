ef0e6a0c867185bb24b6a328d38dfc914c5be53c
kcamelo <153109575+kcamelo@users.noreply.github.com>
2024-01-20 20:48:13 -0300

    20.01.2024_01

--- src/main.js
+++ src/main.js
@@ -4,3 +4,12 @@ import { renderItems } from './view.js';
 import data from './data/dataset.js';
 
 console.log(example, renderItems(data), data);
+
+function realizarPesquisa() {
+    // Obtenha os valores do campo de pesquisa e do filtro de seleção
+    var termoPesquisa = document.getElementById('search-input').value;
+    var filtroSelecao = document.getElementById('filter-select').value;
+
+    // Realize a lógica de pesquisa ou envie os dados para um servidor, conforme necessário
+    alert('Termo de pesquisa: ' + termoPesquisa + '\nFiltro de seleção: ' + filtroSelecao);
+}
