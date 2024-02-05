966eab1c006eecc5964f11a627ef97d5545514fc
kcamelo <153109575+kcamelo@users.noreply.github.com>
2024-02-05 12:31:56 -0300

    tentando fazer subir os cards

--- src/view.js
+++ src/view.js
@@ -1,14 +1,14 @@
 export const renderItems = (data) => {
   const cardAnimal = document.createElement('ul');
   cardAnimal.classlist.add('container');
-  data.forEach(item => {
+  data.forEach((item) => {
     cardAnimal.innerHTML += `
-    <li intemscope itemtype= "croche" class="container">
+    <li intemscope itemtype= "zoologicoAnimals" class="container">
       <dl intemscope itemtype="#hobby">
-        <dt> < img src="${item.imageURL}" alt="imagem do card" itemprop="${item.nameAnimal}" clas="imagem_card" /> </dt>
-        <dt>Nome:</dt> <dd itemprop="name"> ${item.nameAnimal}</dd>
+        <dt> < img src="${item.imageURL}" alt="imagem do card" itemprop="${item.name}" clas="imagem_card" /> </dt>
+        <dt>Nome:</dt> <dd itemprop="name"> ${item.name}</dd>
         <dt>Descrição curta</dt> <dd itemprop="shortDescription"> ${item.shortDescription}</dd>
-        <dt>Descrição</dt> <dd itemprop="description"> ${item.descriptionAnimal}</dd>
+        <dt>Descrição</dt> <dd itemprop="description"> ${item.description}</dd>
         <dt>Fatos</dt> <dd itemprop="facts"> ${item.facts}</dd>
       </dl>
     </li>`
