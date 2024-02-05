e55af166012a20ed80bc2ac26384c51c87539c9a
kcamelo <153109575+kcamelo@users.noreply.github.com>
2024-02-05 11:03:44 -0300

    imagens dos cards

--- src/data/dataset.js
+++ src/data/dataset.js
@@ -4,7 +4,7 @@ export default [
   "name": "Leão",
   "shortDescription": "O majestoso rei da savana",
   "description": "O majestoso leão, conhecido como o rei da selva, destaca-se por sua juba imponente e pelagem dourada. Com olhos penetrantes e poderosas garras, ele personifica a força e a elegância selvagem. Este carnívoro social exibe habilidades de caça excepcionais, liderando orgulhos com destemida autoridade. Seu rugido retumbante ecoa, comunicando domínio e territorialidade. Além de sua natureza feroz, os leões também revelam momentos de ternura, especialmente em interações familiares. Um ícone da savana, o leão representa a imponência da vida selvagem africana.",
-  "imageUrl": "url_da_imagem_leao",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/Le%C3%A3o%201.jpeg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -20,7 +20,7 @@ export default [
   "name": "Tigre",
   "shortDescription": "Elegante felino listrado",
   "description": "O tigre, majestoso felino listrado, destaca-se como um dos maiores do mundo. Com uma presença imponente e listras distintas, é um mestre da caça. Seu habitat diversificado, desde florestas até savanas, testemunha sua adaptabilidade. As características marcantes, como a pelagem elegante e olhos penetrantes, conferem-lhe uma aura de elegância selvagem. O Panthera tigris, pertencente à família Felidae, é essencial para ecossistemas e inspira respeito global.",
-  "imageUrl": "url_da_imagem_tigre",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/Tigre%202.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -36,7 +36,7 @@ export default [
   "name": "Elefante",
   "shortDescription": "Majestoso gigante terrestre",
   "description": "O elefante, gigante gentil da terra, personifica a inteligência e a sociabilidade. Com sua tromba versátil, esses mamíferos emocionais expressam afeto e habilidades incríveis. Seu papel vital na ecologia, juntamente com a memória impressionante, faz deles seres essenciais para os ecossistemas. Seja nas vastas savanas africanas ou nas densas florestas asiáticas, os elefantes, com suas presas distintas, são ícones da vida selvagem.",
-  "imageUrl": "url_da_imagem_elefante",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/Elefante%203.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -52,7 +52,7 @@ export default [
   "name": "Girafa",
   "shortDescription": "Mamífero de pescoço longo",
   "description": "A girafa, com sua elegância única, destaca-se como o gigante gracioso das planícies africanas. Possuindo um pescoço esbelto e manchas distintas, ela é uma visão imponente na savana. A adaptabilidade de suas longas pernas e língua preênsil contribui para sua sobrevivência em ambientes diversos. A Giraffa camelopardalis, símbolo da majestade selvagem, encanta com sua presença serena, transmitindo a beleza inigualável da vida selvagem africana.",
-  "imageUrl": "url_da_imagem_girafa",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/girafa%204.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -68,7 +68,7 @@ export default [
   "name": "Macaco-rhesus",
   "shortDescription": "Primate ágil e inteligente",
   "description": "O macaco-rhesus, também conhecido como macaco de Rhesus, é uma espécie de macaco do Velho Mundo nativo da Ásia. Com pelagem cinza e face rosada, destaca-se como uma espécie altamente sociável. Habitante de uma variedade de habitats, desde florestas até áreas urbanas, demonstra notável adaptabilidade. Sua inteligência e capacidade de aprendizado tornam-no crucial em pesquisas científicas. No entanto, a Macaca mulatta enfrenta desafios de conservação devido à perda de habitat e exploração, destacando a importância da preservação para garantir seu papel vital nos ecossistemas asiáticos.",
-  "imageUrl": "url_da_imagem_macaco",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/macaco%205.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -84,7 +84,7 @@ export default [
   "name": "Gorila",
   "shortDescription": "Primo próximo do ser humano",
   "description": "O gorila, robusto e pacífico, é o maior primata terrestre. Com uma força incrível e laços familiares profundos, esses primatas são essenciais para a coesão social. Habitantess de florestas densas da África Central, os Gorilla gorilla desempenham um papel crucial na preservação desses ecossistemas. Seus olhares expressivos revelam uma complexidade emocional, destacando a importância de proteger esses magníficos animais.",
-  "imageUrl": "url_da_imagem_gorila",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/gorila%206.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -100,7 +100,7 @@ export default [
   "name": "Urso-pardo",
   "shortDescription": "Mamífero omnívoro de porte robusto",
   "description": "O imponente urso pardo (Ursus arctos) é uma espécie distribuída por diversas regiões do hemisfério norte, incluindo América do Norte, Europa e Ásia. Reconhecido por sua pelagem densa, variando de marrom a preto, este carnívoro omnívoro desempenha um papel vital nos ecossistemas montanhosos, florestais e tundrais em que habita. Com habilidades de pesca notáveis, contribui para o equilíbrio ecológico, controlando populações de peixes e outros animais. Apesar dos desafios de conservação, o Ursus arctos personifica a majestade da vida selvagem e a importância da preservação de habitats naturais.",
-  "imageUrl": "url_da_imagem_urso",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/Urso%207.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -116,7 +116,7 @@ export default [
   "name": "Zebra",
   "shortDescription": "Equídeo listrado africano",
   "description": "A zebra, com suas listras distintas, é um ícone da savana africana. Sua pelagem única, uma mistura de preto e branco, cria um padrão hipnotizante. Esses herbívoros sociais contribuem para o equilíbrio ecológico, desempenhando um papel vital na dinâmica das planícies. A Equus zebra, com sua beleza selvagem e comportamento gregário, enriquece as paisagens africanas.",
-  "imageUrl": "url_da_imagem_zebra",
+  "imageUrl": "https://raw.githubusercontent.com/kcamelo/SAP012-dataverse/main/src/data/Imagens_card/zebra%208.jpeg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -148,7 +148,7 @@ export default [
   "name": "Rinoceronte",
   "shortDescription": "Herbívoro com chifre distintivo no nariz",
   "description": "O rinoceronte, com seus chifres imponentes, é um gigante herbívoro crucial para a manutenção do ecossistema. Em meio a ameaças de caça furtiva, os Rinocerotidae enfrentam desafios críticos de conservação. Com sua presença imponente nas savanas africanas e florestas asiáticas, esses animais inspiram ações para proteger não apenas suas vidas, mas a rica biodiversidade de seus habitats.",
-  "imageUrl": "url_da_imagem_rinoceronte",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/57287523/original.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -164,7 +164,7 @@ export default [
   "name": "Lobo",
   "shortDescription": "Carnívoro social e inteligente",
   "description": "O lobo, predador inteligente e social, destaca-se como um símbolo de resistência selvagem. Com pelagem espessa e olhos penetrantes, esses canídeos são mestres da caça em matilhas organizadas. Sua presença é crucial para manter o equilíbrio dos ecossistemas, influenciando positivamente a biodiversidade. O Canis lupus, habita florestas, tundras e desertos, demonstrando adaptação notável em diversas paisagens, tornando-se uma peça fundamental no intricado quebra-cabeça da vida selvagem.",
-  "imageUrl": "url_da_imagem_lobo",
+  "imageUrl": "https://img.freepik.com/fotos-premium/canis-lupus-canis-lupus-lobo-cinzento_410516-45693.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -180,7 +180,7 @@ export default [
   "name": "Águia",
   "shortDescription": "Poderosa ave de rapina com visão aguçada",
   "description": "A águia, majestosa soberana dos céus, personifica a visão aguçada e a destreza aérea. Com asas poderosas e penas imponentes, ela reina sobre montanhas e vales. Símbolo de liberdade e nobreza, as águias desempenham um papel crucial no controle populacional de presas, contribuindo para a saúde dos ecossistemas. Seja sobre penhascos escarpados ou planícies extensas, essas aves de rapina inspiram admiração em todos os que contemplam seus voos majestosos.",
-  "imageUrl": "url_da_imagem_aguia",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/1594/large.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -196,7 +196,7 @@ export default [
   "name": "Caninana-marrom-listrada",
   "shortDescription": "Serpente não venenosa com padrão listrado",
   "description": "A caninana-marrom-listrada é uma serpente não venenosa encontrada em habitats diversos, desde florestas até áreas abertas. Seu padrão de listras marrons ao longo do corpo contribui para sua camuflagem entre a vegetação. Com hábitos ágeis e uma dieta que inclui pequenos vertebrados, a caninana desempenha um papel ecológico importante no controle de populações de roedores. Sua adaptação a ambientes variados destaca sua versatilidade e a diversidade das serpentes na América do Sul.",
-  "imageUrl": "url_da_imagem_caninana",
+  "imageUrl": "https://inpn.mnhn.fr/photos/uploads/webtofs/inpn/ant/90821.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -212,7 +212,7 @@ export default [
   "name": "Cachorro-do-mato",
   "shortDescription": "Canídeo selvagem e ágil",
   "description": "O cachorro-do-mato, canídeo ágil e noturno, é uma presença comum nas paisagens da América do Sul. Com pelagem acinzentada e orelhas pontiagudas, esses predadores desempenham um papel vital no controle de populações de roedores. Sua adaptação a ambientes variados, desde florestas até campos abertos, destaca sua versatilidade. O Cerdocyon thous, apesar de sua semelhança com cães domésticos, personifica a fauna diversificada das Américas",
-  "imageUrl": "url_da_imagem_cachorro_do_mato",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/12204213/original.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -228,15 +228,15 @@ export default [
   "name": "Jaritataca",
   "shortDescription": "Ave exótica de cores vibrantes",
   "description": "A jaritataca, marsupial noturno da América do Sul, destaca-se por sua bolsa ventral e cauda preênsil. Com uma dieta variada e comportamento arbóreo, ela contribui para o controle de populações de insetos e pequenos vertebrados. O Didelphis albiventris, adaptado a habitats variados, exemplifica a biodiversidade única da fauna sul-americana, revelando a beleza intrínseca dos marsupiais noturnos.",
-  "imageUrl": "url_da_imagem_jaritataca",
+  "imageUrl": "https://animalbusiness.com.br/wp-content/uploads/2022/03/Fig-1_jaritataca-1.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
-  "Classe": "Aves",
-  "Ordem": "Psittaciformes",
-  "Família": "Psittacidae",
-  "Gênero": "Ara",
-  "Espécie": "ararauna"
+  "Classe": "Mammalia",
+  "Ordem": "Carnivora ",
+  "Família": "Mephitidae",
+  "Gênero": "Conepatus",
+  "Espécie": "Conepatus semistriatus"
   }
   },
   {
@@ -244,7 +244,7 @@ export default [
   "name": "Tucanuçu",
   "shortDescription": "Ave com bico longo e colorido",
   "description": "O tucanuçu, majestoso representante das aves tropicais, destaca-se por seu bico vibrante e colorido. Nativos das florestas da América do Sul, essas aves desempenham um papel vital na dispersão de sementes, influenciando a regeneração florestal. O Ramphastos toco, com seu voo gracioso e vocalizações distintas, é um embaixador da exuberância da biodiversidade nas selvas tropicais, capturando a imaginação de observadores de aves em todo o mundo.",
-  "imageUrl": "url_da_imagem_tucanucu",
+  "imageUrl": "https://s3.amazonaws.com/media.wikiaves.com.br/images/1431/1341879_ad749b616252b5c49edeb240f938d3d9.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -260,7 +260,7 @@ export default [
   "name": "Tamanduá-mirim",
   "shortDescription": "Formigueiro com língua extensível",
   "description": "O tamanduá-mirim, tamanduá de pequeno porte da América do Sul, exibe uma língua extensível e garras poderosas. Com hábitos arbóreos e terrestres, é uma peça essencial nos ecossistemas, controlando populações de insetos. O Tamandua tetradactyla, apesar de sua aparência singular, personifica a adaptação única da fauna sul-americana, ilustrando a incrível diversidade dos mamíferos que habitam florestas e savanas.",
-  "imageUrl": "url_da_imagem_tamandua_mirim",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/10425251/large.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -276,7 +276,7 @@ export default [
   "name": "Jabuti-piranga",
   "shortDescription": "Réptil terrestre com carapaça",
   "description": "O jabuti-piranga, réptil terrestre e herbívoro, é uma presença marcante nas florestas da América do Sul. Com sua carapaça resistente e cores vibrantes, ele personifica a longevidade e a paciência. O Chelonoidis carbonaria, desempenhando um papel ecológico ao dispersar sementes, destaca-se como um ícone da biodiversidade e da resiliência dos quelônios na América do Sul.",
-  "imageUrl": "url_da_imagem_jabuti_piranga",
+  "imageUrl": "https://www.infoescola.com/wp-content/uploads/2011/02/jabuti-chelonoidis-carbonaria-493355596.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -292,7 +292,7 @@ export default [
   "name": "Preguiça",
   "shortDescription": "Mamífero arbóreo conhecido pela lentidão",
   "description": "A preguiça, icônica residente das florestas tropicais, destaca-se por seus movimentos lentos e comportamento arbóreo. Com garras adaptadas para pendurar-se em árvores, esses mamíferos herbívoros contribuem para a saúde das florestas ao dispersar sementes. O gênero Bradypus, com suas três espécies, personifica a adaptabilidade única dos preguiçosos, representando a excentricidade e importância da fauna sul-americana.",
-  "imageUrl": "url_da_imagem_preguica",
+  "imageUrl": "https://static.inaturalist.org/photos/73892662/large.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -308,7 +308,7 @@ export default [
   "name": "Raposa-do-campo",
   "shortDescription": "Canídeo adaptado a ambientes abertos",
   "description": "A raposa-do-campo, canídeo ágil da América do Sul, destaca-se por sua pelagem acinzentada e comportamento noturno. Com adaptações para ambientes abertos, essas raposas desempenham um papel crucial no controle de populações de roedores. O Lycalopex gymnocercus, com suas características distintas, exemplifica a diversidade da fauna sul-americana e sua capacidade de prosperar em paisagens variadas.",
-  "imageUrl": "url_da_imagem_raposa_do_campo",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/29524344/large.jpeg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -324,7 +324,7 @@ export default [
   "name": " cobra-de-cipo-verde ",
   "shortDescription": "Réptil com escamas transparentes",
   "description": "A cobra-de-cipo-verde é uma serpente arbórea encontrada nas Américas Central e do Sul. Seu nome reflete a cor vibrante de sua pele, que mescla tons de verde. Adaptada à vida nas árvores, é ágil e graciosa em seus movimentos. Com uma dieta que inclui pequenos vertebrados, essa serpente desempenha um papel na regulação populacional. A Oxybelis fulgidus personifica a elegância da fauna tropical, revelando a diversidade ecológica das regiões onde habita.",
-  "imageUrl": "url_da_imagem_cobra_de_vidro",
+  "imageUrl": "https://s2.glbimg.com/u4cGmHiJcRyfmzDLjLZHi18Z4yY=/s.glbimg.com/jo/g1/f/original/2013/10/04/cobra2_1.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -339,7 +339,7 @@ export default [
   "name": "Lobo-guará",
   "shortDescription": "Canídeo nativo da América do Sul",
   "description": "O lobo-guará é um canídeo nativo da América do Sul, notável por sua pelagem avermelhada e pernas longas. Habita as savanas e cerrados, sendo uma espécie adaptada a ambientes abertos. Solitário e omnívoro, o lobo-guará se alimenta de frutas, pequenos vertebrados e insetos. Sua presença desempenha um papel crucial no equilíbrio ecológico dessas paisagens. A Chrysocyon brachyurus, além de sua singularidade física, representa a riqueza da biodiversidade sul-americana e a importância da conservação desses habitats.",
-  "imageUrl": "url_da_imagem_lobo_guara",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/297930439/original.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -355,7 +355,7 @@ export default [
   "name": "Harpia",
   "shortDescription": "Ave de rapina com garras poderosas",
   "description": "A harpia é uma majestosa águia encontrada nas florestas tropicais da América Central e do Sul. Com uma envergadura impressionante e penas distintas, ela personifica a grandiosidade das aves de rapina. Como predadora de topo, a harpia desempenha um papel crucial na manutenção do equilíbrio ecológico ao regular as populações de mamíferos e aves em suas áreas de habitat. Seu nome é uma homenagem à mitologia grega, representando a beleza e a força dessa ave, que simboliza a imponência das florestas tropicais sul-americanas.",
-  "imageUrl": "url_da_imagem_harpia",
+  "imageUrl": "https://www.infoescola.com/wp-content/uploads/2016/08/gavi%C3%A3o_156460400.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -371,7 +371,7 @@ export default [
   "name": "Capivara",
   "shortDescription": "Maior roedor do mundo",
   "description": "A capivara, maior roedor do mundo, é uma presença carismática nas paisagens aquáticas da América do Sul. Com sua pelagem densa e hábitos semi-aquáticos, ela personifica a harmonia entre ecossistemas aquáticos e terrestres. Social e pacífica, a Hydrochoerus hydrochaeris contribui para a biodiversidade ao controlar a vegetação aquática. Sua adaptação única a diferentes ambientes, desde pântanos até campos abertos, destaca a versatilidade e a importância desses roedores na fauna sul-americana.",
-  "imageUrl": "url_da_imagem_capivara",
+  "imageUrl": "https://inaturalist-open-data.s3.amazonaws.com/photos/91546944/large.jpg",
   "facts": {
   "Reino": "Animalia",
   "Filo": "Chordata",
@@ -379,7 +379,7 @@ export default [
   "Ordem": "Rodentia",
   "Família": "Caviidae",
   "Gênero": "Hydrochoerus",
-  "Espécie": "hHydrochoerus hydrochaeris"
+  "Espécie": "Hydrochoerus hydrochaeris"
   }
   }
   ]
