0c90f6d1098399d5070db1ba27a83813e9bd76b8
kcamelo <153109575+kcamelo@users.noreply.github.com>
2024-02-21 15:51:04 -0300

    css e datafunctions

--- src/dataFunctions.js
+++ src/dataFunctions.js
@@ -1,9 +1,19 @@
 // Estas funciones son ejemplos, aquí puedes desarrollar tus propias funciones.
 
-export const example = () => {
-  return 'example';
+export const filterData = (data, FilterBy, value) => {
+  const dadosFiltrados = data.filter(
+    (ordem) => ordem.facts.ordemAnimal === value
+  );
 };
 
 export const anotherExample = () => {
   return [];
 };
+// renderizar cards pelo root 
+const listaCards = document.querySelector("#root")
+listaCards.innerHTML = renderItems (data);
+
+let cardsFiltrados = data;
+
+// Filtro de Ordem 
+const filtroOrdem = 
